64f490c500da745be2fde7b8c5eeaa20566b234f
WINDOWS-72MUN56\User <aryandh.a.w@gmail.com>
2022-06-16 20:26:57 +0700

    trial error get all user&family

--- src/main/java/com/altera/capstone/bookingvaccine/controller/UserController.java
+++ src/main/java/com/altera/capstone/bookingvaccine/controller/UserController.java
@@ -40,6 +40,17 @@ public class UserController {
 //        return ResponseEntity.ok(principal.getName()+" Berhasil Login");
 //    }
 
+  // GET By ID User for GET Family
+//  @ApiOperation(value = "GET User by id for GET All Family",  response = UserDto.class)
+//  @ApiResponses(value = {
+//          @ApiResponse(code = 200, message = "Success GET User by id for GET All Family"),
+//  })
+//  @GetMapping(value = "/allMember/{id}")
+//  public ResponseEntity<Object> getByAllMemberId(@PathVariable(value = "id") Long id_user){
+//    return userService.getFamilyByUserId(id_user);
+//  }
+
+
   // POST
   @ApiOperation(value = "Register user",  response = UsernamePassword.class)
   @ApiResponses(value = {
@@ -96,56 +107,5 @@ public class UserController {
   public ResponseEntity<Object> deleteUser(@PathVariable(value = "id") Long id) {
     return userService.deleteUser(id);
   }
-//  @ApiOperation(value = "upload photo user",  response = UsernamePassword.class)
-//  @ApiResponses(value = {
-//          @ApiResponse(code = 200, message = "Success upload photo user"),
-//
-//  })
-//  @PostMapping("/upload")
-//  public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file){
-//    String message = "";
-//    try {
-//      userService.store(file);
-//      message = "Uploaded The File Successfully: "+ file.getOriginalFilename();
-//      return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
-//    } catch (Exception e){
-//      message = "Could Not Upload the File : "+ file.getOriginalFilename() + "!";
-//      return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
-//    }
-//  }
 
-//    @PostMapping("/upload/image")
-//    public ResponseEntity<ImageUploadResponse> uplaodImage(@RequestParam("image") MultipartFile file)
-//            throws IOException {
-//
-//        userRepository.save(UserDao.builder()
-//                .name(file.getOriginalFilename())
-//                .typeImg(file.getContentType())
-//                .imageProfile(ImageUtility.compressImage(file.getBytes())).build());
-//        return ResponseEntity.status(HttpStatus.OK)
-//                .body(new ImageUploadResponse("Image uploaded successfully: " +
-//                        file.getOriginalFilename()));
-//    }
-
-//    @GetMapping(path = {"/get/image/info/{name}"})
-//    public UserDao getImageDetails(@PathVariable("name") String namePhoto) throws IOException {
-//
-//        final Optional<UserDao> dbImage = userRepository.findByName(namePhoto);
-//
-//        return UserDao.builder()
-//                .name(dbImage.get().getName())
-//                .typeImg(dbImage.get().getTypeImg())
-//                .imageProfile(ImageUtility.decompressImage(dbImage.get().getImageProfile())).build();
-//    }
-
-//    @GetMapping(path = {"/get/image/{name}"})
-//    public ResponseEntity<byte[]> getImage(@PathVariable("name") String namePhoto) throws IOException {
-//
-//        final Optional<UserDao> dbImage = userRepository.findByName(namePhoto);
-//
-//        return ResponseEntity
-//                .ok()
-//                .contentType(MediaType.valueOf(dbImage.get().getTypeImg()))
-//                .body(ImageUtility.decompressImage(dbImage.get().getImageProfile()));
-//    }
 }
