a06e157b7e509f35fdd3a2d94658dfea27cc49c4
WINDOWS-72MUN56\User <aryandh.a.w@gmail.com>
2022-06-17 14:57:14 +0700

    add query Get facility By UserId

--- src/main/java/com/altera/capstone/bookingvaccine/controller/HealthFacilitiesController.java
+++ src/main/java/com/altera/capstone/bookingvaccine/controller/HealthFacilitiesController.java
@@ -2,6 +2,7 @@ package com.altera.capstone.bookingvaccine.controller;
 
 import com.altera.capstone.bookingvaccine.domain.dto.FamilyDto;
 import com.altera.capstone.bookingvaccine.domain.dto.HealthFaciltiesDto;
+import com.altera.capstone.bookingvaccine.domain.dto.SessionDto;
 import com.altera.capstone.bookingvaccine.service.HealthFacilitiesService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -30,6 +31,17 @@ public class HealthFacilitiesController {
   @GetMapping(value = "")
   public ResponseEntity<Object> getAll() {
     return healthFacilitiesService.getAllHealthFacility();
+  }
+
+  // GET By UserId // Filter for manage schedule vaccine
+  @ApiOperation(value = "Get facility by User id",  response = SessionDto.class)
+  @ApiResponses(value = {
+          @ApiResponse(code = 200, message = "Success get facility by User id"),
+
+  })
+  @GetMapping(value = "/user/{id}")
+  public ResponseEntity<Object> getByAreaId(@PathVariable(value = "id") Long id){
+    return healthFacilitiesService.getFacilityByUserId(id);
   }
 
   // POST Family
