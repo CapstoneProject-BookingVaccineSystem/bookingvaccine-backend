71a38264a71be781bb8b5d0c76cd32e7f0565f70
Abdullah Akbar <abdullahakbar419@gmail.com>
2022-07-16 11:17:01 +0700

    Fixing respone Get All Session Pagination request FE

--- src/main/java/com/altera/capstone/bookingvaccine/controller/SessionController.java
+++ src/main/java/com/altera/capstone/bookingvaccine/controller/SessionController.java
@@ -23,28 +23,30 @@ import java.time.LocalTime;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping(value = "/v1/session", produces = MediaType.APPLICATION_JSON_VALUE)
-@Api(tags = "Session", value = "Session" )
+@Api(tags = "Session", value = "Session")
 public class SessionController {
   @Autowired
   private SessionService sessionService;
 
-//  Sort, fulltext search
+  // Sort, fulltext search
   // GET All with Pageable
-  @ApiOperation(value = "Get all session",  response = SessionDto.class)
+  @ApiOperation(value = "Get all session", response = SessionDto.class)
   @ApiResponses(value = {
-          @ApiResponse(code = 200, message = "Success get list session"),
+      @ApiResponse(code = 200, message = "Success get list session"),
 
   })
-  @GetMapping(value = "/{page}/{size}")
-  public ResponseEntity<Object> getAll(@PathVariable(value = "page") int page,
-                                       @PathVariable(value = "size") int size) {
-    return sessionService.getAllSession(page, size);
+  @GetMapping(value = "/paging")
+  public ResponseEntity<Object> getAll(
+      @RequestParam(value = "page") int page,
+      @RequestParam(value = "size") int size) {
+
+    return sessionService.getAllSessionPagination(page, size);
   }
 
   // GET ALL
-  @ApiOperation(value = "Get all Session",  response = AreaDto.class)
+  @ApiOperation(value = "Get all Session", response = AreaDto.class)
   @ApiResponses(value = {
-          @ApiResponse(code = 200, message = "Success get list Session"),
+      @ApiResponse(code = 200, message = "Success get list Session"),
   })
   @GetMapping(value = "")
   public ResponseEntity<Object> getAll() {
@@ -52,80 +54,77 @@ public class SessionController {
   }
 
   // GET By Like
-  @ApiOperation(value = "Get By LIKE facility OR vaccine",  response = SessionDto.class)
+  @ApiOperation(value = "Get By LIKE facility OR vaccine", response = SessionDto.class)
   @ApiResponses(value = {
-          @ApiResponse(code = 200, message = "Success get By LIKE facility OR vaccine"),
+      @ApiResponse(code = 200, message = "Success get By LIKE facility OR vaccine"),
 
   })
   @GetMapping("/search/{search}")
-  public ResponseEntity<Object> getSearch(@PathVariable(value = "search") String search){
+  public ResponseEntity<Object> getSearch(@PathVariable(value = "search") String search) {
     return sessionService.getFacilityByLike(search);
   }
 
   // GETById
-  @ApiOperation(value = "Get session by id",  response = SessionDto.class)
+  @ApiOperation(value = "Get session by id", response = SessionDto.class)
   @ApiResponses(value = {
-          @ApiResponse(code = 200, message = "Success get session by id"),
+      @ApiResponse(code = 200, message = "Success get session by id"),
 
   })
   @GetMapping(value = "/{id}")
-  public ResponseEntity<Object> getById(@PathVariable(value = "id") Long id){
+  public ResponseEntity<Object> getById(@PathVariable(value = "id") Long id) {
     return sessionService.getSessionById(id);
   }
 
   // GET By AreaId // Filter for mobile app
-  @ApiOperation(value = "Get session by Area id",  response = SessionDto.class)
+  @ApiOperation(value = "Get session by Area id", response = SessionDto.class)
   @ApiResponses(value = {
-          @ApiResponse(code = 200, message = "Success get session by id"),
+      @ApiResponse(code = 200, message = "Success get session by id"),
 
   })
   @GetMapping(value = "/area/{id}")
-  public ResponseEntity<Object> getByAreaId(@PathVariable(value = "id") Long id){
+  public ResponseEntity<Object> getByAreaId(@PathVariable(value = "id") Long id) {
     return sessionService.getSessionByAreaId(id);
   }
 
   // POST
-  @ApiOperation(value = "Add session",  response = SessionDto.class)
+  @ApiOperation(value = "Add session", response = SessionDto.class)
   @ApiResponses(value = {
-          @ApiResponse(code = 200, message = "Success add session"),
+      @ApiResponse(code = 200, message = "Success add session"),
 
   })
   @PostMapping(value = "")
   public ResponseEntity<Object> addSession(@RequestParam(value = "vaccine_id") Long vaccine_id,
-                                           @RequestParam(value = "area_id") Long area_id,
-                                           @RequestParam(value = "health_facilities_id") Long health_facilities_id,
-                                           @RequestParam(value = "stock") Integer stock,
-                                           @RequestParam(value = "start_date")
-                                             @DateTimeFormat(pattern = "yyyy-MM-dd")  LocalDate start_date,
-                                           @RequestParam(value = "start_time")
-                                             @DateTimeFormat(pattern = "HH:mm") LocalTime start_time,
-                                           @RequestParam(value = "file", required = false) MultipartFile multipartFile) throws IOException {
+      @RequestParam(value = "area_id") Long area_id,
+      @RequestParam(value = "health_facilities_id") Long health_facilities_id,
+      @RequestParam(value = "stock") Integer stock,
+      @RequestParam(value = "start_date") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate start_date,
+      @RequestParam(value = "start_time") @DateTimeFormat(pattern = "HH:mm") LocalTime start_time,
+      @RequestParam(value = "file", required = false) MultipartFile multipartFile) throws IOException {
 
-    return sessionService.addSessionWithPhoto(vaccine_id, area_id, health_facilities_id, stock, start_date, start_time, multipartFile);
+    return sessionService.addSessionWithPhoto(vaccine_id, area_id, health_facilities_id, stock, start_date, start_time,
+        multipartFile);
   }
 
   // PUT
-  @ApiOperation(value = "Update session",  response = SessionDto.class)
+  @ApiOperation(value = "Update session", response = SessionDto.class)
   @ApiResponses(value = {
-          @ApiResponse(code = 200, message = "Success Update session"),
+      @ApiResponse(code = 200, message = "Success Update session"),
 
   })
   @PutMapping(value = "/{id}")
   public ResponseEntity<Object> updateSession(@PathVariable(value = "id") Long id,
-                                              @RequestParam(value = "vaccine_id") Long vaccine_id,
-                                              @RequestParam(value = "stock") Integer stock,
-                                              @RequestParam(value = "start_date")
-                                                @DateTimeFormat(pattern = "yyyy-MM-dd")  LocalDate start_date,
-                                              @RequestParam(value = "start_time")
-                                                @DateTimeFormat(pattern = "HH:mm") LocalTime start_time,
-                                              @RequestParam(value = "file", required = false) MultipartFile multipartFile) throws IOException {
+      @RequestParam(value = "vaccine_id") Long vaccine_id,
+      @RequestParam(value = "stock") Integer stock,
+      @RequestParam(value = "start_date") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate start_date,
+      @RequestParam(value = "start_time") @DateTimeFormat(pattern = "HH:mm") LocalTime start_time,
+      @RequestParam(value = "file", required = false) MultipartFile multipartFile) throws IOException {
     return sessionService.updateSession(id, vaccine_id, stock, start_date, start_time, multipartFile);
   }
 
   // DELETE
-  @ApiOperation(value = "Delete session",  response = SessionDto.class)
+  @ApiOperation(value = "Delete session", response = SessionDto.class)
   @ApiResponses(value = {
-          @ApiResponse(code = 200, message = "Success delete session"),
+      @ApiResponse(code = 200, message = "Success delete session"),
 
   })
   @DeleteMapping(value = "/{id}")
--- src/main/java/com/altera/capstone/bookingvaccine/service/SessionService.java
+++ src/main/java/com/altera/capstone/bookingvaccine/service/SessionService.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -53,12 +54,13 @@ public class SessionService {
   @Value("${booking-api.url}")
   private String apiUrl;
 
-  public ResponseEntity<Object> getAllSession(int page, int size) {
+  public ResponseEntity<Object> getAllSessionPagination(int page, int size) {
     log.info("Executing get all session.");
-    try{
-      Pageable paging = PageRequest.of(page, size);
+    try {
+      Pageable paging = PageRequest.of(page, size,
+          Sort.by("startDate").descending().and(Sort.by("startTime").descending()));
       Page<SessionDao> pageResult = sessionRepository.findAll(paging);
-      return ResponseUtil.build(AppConstant.Message.SUCCESS, pageResult.toList(), HttpStatus.OK);
+      return ResponseUtil.build(AppConstant.Message.SUCCESS, pageResult, HttpStatus.OK);
     } catch (Exception e) {
       log.error("Happened error when get all session. Error: {}", e.getMessage());
       log.trace("Get error when get all session. ", e);
@@ -68,7 +70,7 @@ public class SessionService {
 
   public ResponseEntity<Object> getAll() {
     log.info("Executing get all session.");
-    try{
+    try {
       List<SessionDao> sessionDaoList = (List<SessionDao>) sessionRepository.findAll();
       return ResponseUtil.build(AppConstant.Message.SUCCESS, sessionDaoList, HttpStatus.OK);
     } catch (Exception e) {
@@ -83,7 +85,7 @@ public class SessionService {
     log.info("Executing get session by area id: {} ", id);
     try {
       List<SessionDao> sessionDao = sessionRepository.getSessionByAreaId(id);
-      if(sessionDao.isEmpty()) {
+      if (sessionDao.isEmpty()) {
         log.info("Area id: {} not found", id);
         return ResponseUtil.build(AppConstant.Message.NOT_FOUND, null, HttpStatus.BAD_REQUEST);
       }
@@ -97,12 +99,12 @@ public class SessionService {
   }
 
   // GET Data with LIKE Sorting
-  public ResponseEntity<Object> getFacilityByLike(String search){
+  public ResponseEntity<Object> getFacilityByLike(String search) {
     try {
       log.info("Execute get data facility");
       List<SessionDao> sessionDaoList = sessionRepository.findByFacilityLike(search, search);
       return ResponseUtil.build(AppConstant.Message.SUCCESS, sessionDaoList, HttpStatus.OK);
-    } catch (Exception e){
+    } catch (Exception e) {
       log.error("Happened error when get session by id. Error: {}", e.getMessage());
       log.trace("Get error when get session by id. ", e);
       throw e;
@@ -113,7 +115,7 @@ public class SessionService {
     log.info("Executing get session by id: {} ", id);
     try {
       Optional<SessionDao> sessionDao = sessionRepository.findById(id);
-      if(sessionDao.isEmpty()) {
+      if (sessionDao.isEmpty()) {
         log.info("session id: {} not found", id);
         return ResponseUtil.build(AppConstant.Message.NOT_FOUND, null, HttpStatus.BAD_REQUEST);
       }
@@ -126,79 +128,88 @@ public class SessionService {
     }
   }
 
-//  public ResponseEntity<Object> addSession(SessionDto request) {
-//    log.info("Executing add session with request: {}", request);
-//    try{
-//
-//      log.info("Get area by id: {}", request.getIdArea());
-//      Optional<AreaDao> areaDaoOptional = areaRepository.findById(request.getIdArea());
-//      if (areaDaoOptional.isEmpty()) {
-//        log.info("area [{}] not found", request.getIdArea());
-//        return ResponseUtil.build(AppConstant.Message.NOT_FOUND, null, HttpStatus.BAD_REQUEST);
-//      }
-//
-//      log.info("Get health facility by id: {}", request.getIdHealthFacilities());
-//      Optional<HealthFacilitiesDao> healthFacilitiesDaoOptional = healthFacilitesRepository.findById(request.getIdHealthFacilities());
-//      if (healthFacilitiesDaoOptional.isEmpty()) {
-//        log.info("health facility [{}] not found", request.getIdHealthFacilities());
-//        return ResponseUtil.build(AppConstant.Message.NOT_FOUND, null, HttpStatus.BAD_REQUEST);
-//      }
-//
-//      log.info("Get vaccine by id: {}", request.getIdVaccine());
-//      Optional<VaccineDao> vaccineDaoOptional = vaccineRepository.findById(request.getIdVaccine());
-//      if (vaccineDaoOptional.isEmpty()) {
-//        log.info("vaccine [{}] not found", request.getIdVaccine());
-//        return ResponseUtil.build(AppConstant.Message.NOT_FOUND, null, HttpStatus.BAD_REQUEST);
-//      }
-//
-//      SessionDao sessionDao = SessionDao.builder()
-//              .areaMapped(areaDaoOptional.get())
-//              .vaccineMapped(vaccineDaoOptional.get())
-//              .healthFacilitiesDaoMapped(healthFacilitiesDaoOptional.get())
-//              .stock(request.getStock())
-//              .startDate(request.getStartDate())
-//              .startTime(request.getStartTime())
-////              .lastStock(request.getLastStock())
-//              .build();
-//      sessionDao = sessionRepository.save(sessionDao);
-//      log.info("Executing add session success");
-//      return ResponseUtil.build(AppConstant.Message.SUCCESS, mapper.map(sessionDao, SessionDto.class), HttpStatus.OK);
-//
-//    } catch (Exception e) {
-//      log.error("Happened error when add session. Error: {}", e.getMessage());
-//      log.trace("Get error when add session. ", e);
-//      throw e;
-//    }
-//  }
+  // public ResponseEntity<Object> addSession(SessionDto request) {
+  // log.info("Executing add session with request: {}", request);
+  // try{
+  //
+  // log.info("Get area by id: {}", request.getIdArea());
+  // Optional<AreaDao> areaDaoOptional =
+  // areaRepository.findById(request.getIdArea());
+  // if (areaDaoOptional.isEmpty()) {
+  // log.info("area [{}] not found", request.getIdArea());
+  // return ResponseUtil.build(AppConstant.Message.NOT_FOUND, null,
+  // HttpStatus.BAD_REQUEST);
+  // }
+  //
+  // log.info("Get health facility by id: {}", request.getIdHealthFacilities());
+  // Optional<HealthFacilitiesDao> healthFacilitiesDaoOptional =
+  // healthFacilitesRepository.findById(request.getIdHealthFacilities());
+  // if (healthFacilitiesDaoOptional.isEmpty()) {
+  // log.info("health facility [{}] not found", request.getIdHealthFacilities());
+  // return ResponseUtil.build(AppConstant.Message.NOT_FOUND, null,
+  // HttpStatus.BAD_REQUEST);
+  // }
+  //
+  // log.info("Get vaccine by id: {}", request.getIdVaccine());
+  // Optional<VaccineDao> vaccineDaoOptional =
+  // vaccineRepository.findById(request.getIdVaccine());
+  // if (vaccineDaoOptional.isEmpty()) {
+  // log.info("vaccine [{}] not found", request.getIdVaccine());
+  // return ResponseUtil.build(AppConstant.Message.NOT_FOUND, null,
+  // HttpStatus.BAD_REQUEST);
+  // }
+  //
+  // SessionDao sessionDao = SessionDao.builder()
+  // .areaMapped(areaDaoOptional.get())
+  // .vaccineMapped(vaccineDaoOptional.get())
+  // .healthFacilitiesDaoMapped(healthFacilitiesDaoOptional.get())
+  // .stock(request.getStock())
+  // .startDate(request.getStartDate())
+  // .startTime(request.getStartTime())
+  //// .lastStock(request.getLastStock())
+  // .build();
+  // sessionDao = sessionRepository.save(sessionDao);
+  // log.info("Executing add session success");
+  // return ResponseUtil.build(AppConstant.Message.SUCCESS, mapper.map(sessionDao,
+  // SessionDto.class), HttpStatus.OK);
+  //
+  // } catch (Exception e) {
+  // log.error("Happened error when add session. Error: {}", e.getMessage());
+  // log.trace("Get error when add session. ", e);
+  // throw e;
+  // }
+  // }
 
   public ResponseEntity<Object> addSessionWithPhoto(Long vaccine_id, Long area_id, Long health_facilities_id,
-                                                    Integer stock, LocalDate start_date, LocalTime start_time,
-                                                    MultipartFile multipartFile) throws IOException {
+      Integer stock, LocalDate start_date, LocalTime start_time,
+      MultipartFile multipartFile) throws IOException {
     log.info("Executing add session with request: {}");
-    try{
+    try {
       Optional<AreaDao> areaDaoOptional = areaRepository.findById(area_id);
       if (areaDaoOptional.isEmpty()) {
         return ResponseUtil.build(AppConstant.Message.NOT_FOUND, "area_id NOT FOUND", HttpStatus.BAD_REQUEST);
       }
-      Optional<HealthFacilitiesDao> healthFacilitiesDaoOptional = healthFacilitesRepository.findById(health_facilities_id);
+      Optional<HealthFacilitiesDao> healthFacilitiesDaoOptional = healthFacilitesRepository
+          .findById(health_facilities_id);
       if (healthFacilitiesDaoOptional.isEmpty()) {
-        return ResponseUtil.build(AppConstant.Message.NOT_FOUND, "health_facility_id NOT FOUND", HttpStatus.BAD_REQUEST);
+        return ResponseUtil.build(AppConstant.Message.NOT_FOUND, "health_facility_id NOT FOUND",
+            HttpStatus.BAD_REQUEST);
       }
       Optional<VaccineDao> vaccineDaoOptional = vaccineRepository.findById(vaccine_id);
       if (vaccineDaoOptional.isEmpty()) {
         return ResponseUtil.build(AppConstant.Message.NOT_FOUND, "vaccine_id NOT FOUND", HttpStatus.BAD_REQUEST);
       }
 
-      if(multipartFile == null) {
+      if (multipartFile == null) {
         SessionDao sessionDao = SessionDao.builder()
-                .areaMapped(areaDaoOptional.get())
-                .vaccineMapped(vaccineDaoOptional.get())
-                .healthFacilitiesDaoMapped(healthFacilitiesDaoOptional.get())
-                .stock(stock)
-                .startDate(start_date)
-                .startTime(start_time)
-//              .lastStock(request.getLastStock())
-                .build();
+            .areaMapped(areaDaoOptional.get())
+            .vaccineMapped(vaccineDaoOptional.get())
+            .healthFacilitiesDaoMapped(healthFacilitiesDaoOptional.get())
+            .stock(stock)
+            .startDate(start_date)
+            .startTime(start_time)
+            // .lastStock(request.getLastStock())
+            .build();
         sessionDao = sessionRepository.save(sessionDao);
         log.info("Executing add session without photo success");
         return ResponseUtil.build(AppConstant.Message.SUCCESS, mapper.map(sessionDao, SessionDto.class), HttpStatus.OK);
@@ -209,17 +220,17 @@ public class SessionService {
       String filecode = FileUploadUtil.saveFile(fileName, multipartFile);
 
       SessionDao sessionDao = SessionDao.builder()
-              .areaMapped(areaDaoOptional.get())
-              .vaccineMapped(vaccineDaoOptional.get())
-              .healthFacilitiesDaoMapped(healthFacilitiesDaoOptional.get())
-              .stock(stock)
-              .startDate(start_date)
-              .startTime(start_time)
-              .fileName(fileName)
-              .size(size)
-              .image(apiUrl + "/images/" + filecode)
-//              .lastStock(request.getLastStock())
-              .build();
+          .areaMapped(areaDaoOptional.get())
+          .vaccineMapped(vaccineDaoOptional.get())
+          .healthFacilitiesDaoMapped(healthFacilitiesDaoOptional.get())
+          .stock(stock)
+          .startDate(start_date)
+          .startTime(start_time)
+          .fileName(fileName)
+          .size(size)
+          .image(apiUrl + "/images/" + filecode)
+          // .lastStock(request.getLastStock())
+          .build();
       sessionDao = sessionRepository.save(sessionDao);
       log.info("Executing add session success");
       return ResponseUtil.build(AppConstant.Message.SUCCESS, mapper.map(sessionDao, SessionDto.class), HttpStatus.OK);
@@ -232,41 +243,42 @@ public class SessionService {
   }
 
   public ResponseEntity<Object> updateSession(Long id, Long vaccine_id, Integer stock,
-                                              LocalDate start_date, LocalTime start_time,
-                                              MultipartFile multipartFile) throws IOException{
+      LocalDate start_date, LocalTime start_time,
+      MultipartFile multipartFile) throws IOException {
     log.info("Executing update session with request: {}");
     try {
       Optional<SessionDao> sessionDaoOptional = sessionRepository.findById(id);
-      if(sessionDaoOptional.isEmpty()) {
+      if (sessionDaoOptional.isEmpty()) {
         log.info("session {} not found", id);
         return ResponseUtil.build(AppConstant.Message.NOT_FOUND, null, HttpStatus.BAD_REQUEST);
       }
-//      String msg = "session {} not found";
+      // String msg = "session {} not found";
 
       String fileName = StringUtils.cleanPath(Objects.requireNonNull(multipartFile.getOriginalFilename()));
       long size = multipartFile.getSize();
       String filecode = FileUploadUtil.saveFile(fileName, multipartFile);
 
       sessionDaoOptional.ifPresent(res -> {
-        if (multipartFile == null){
-          res.setVaccineMapped(new VaccineDao(vaccine_id)); //updated vaccine //buat kondisi dengan tenary
+        if (multipartFile == null) {
+          res.setVaccineMapped(new VaccineDao(vaccine_id)); // updated vaccine //buat kondisi dengan tenary
           res.setStartDate(start_date);
           res.setStartTime(start_time);
           res.setStock(stock);
           sessionRepository.save(res);
         }
-        res.setVaccineMapped(new VaccineDao(vaccine_id)); //updated vaccine //buat kondisi dengan tenary
+        res.setVaccineMapped(new VaccineDao(vaccine_id)); // updated vaccine //buat kondisi dengan tenary
         res.setStartDate(start_date);
         res.setStartTime(start_time);
         res.setStock(stock);
         res.setFileName(fileName);
         res.setSize(size);
         res.setImage(apiUrl + "/images/" + filecode);
-//        res.setLastStock(request.getLastStock());
+        // res.setLastStock(request.getLastStock());
         sessionRepository.save(res);
       });
       log.info("Executing update session success");
-      return ResponseUtil.build(AppConstant.Message.SUCCESS, mapper.map(sessionDaoOptional, SessionDto.class), HttpStatus.OK);
+      return ResponseUtil.build(AppConstant.Message.SUCCESS, mapper.map(sessionDaoOptional, SessionDto.class),
+          HttpStatus.OK);
     } catch (Exception e) {
       log.error("Happened error when update session. Error: {}", e.getMessage());
       log.trace("Get error when update session. ", e);
@@ -276,9 +288,9 @@ public class SessionService {
 
   public ResponseEntity<Object> deleteSession(Long id) {
     log.info("Executing delete session id: {}", id);
-    try{
+    try {
       Optional<SessionDao> sessionDaoOptional = sessionRepository.findById(id);
-      if(sessionDaoOptional.isEmpty()) {
+      if (sessionDaoOptional.isEmpty()) {
         log.info("session {} not found", id);
         return ResponseUtil.build(AppConstant.Message.NOT_FOUND, "Session not found !", HttpStatus.BAD_REQUEST);
       }
